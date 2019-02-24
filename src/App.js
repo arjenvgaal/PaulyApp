5da9328e68ca1b71e4a38b8e4414f7f8561cb5e2
gmverdon <gmverdon@users.noreply.github.com>
2019-02-24 14:24:07 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,5 @@ import React, { Component } from 'react';
 import NavigationRouter from './utils/routes';
 
 export default class App extends Component {
-  render = () => (
-    <NavigationRouter />
-  );
+  render = () => <NavigationRouter />;
 };
